18c9b5e7ab944a4b13b2329600ca25e4b0c9a743
farid <farid@LAPTOP-SPTVSQVM>
2024-08-15 09:40:40 +0530

    Add project files.

---
+++ Assignment10 in CS/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignment10
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Multiplication Table for " + i + ":");
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine(i + " x " + j + " = " + (i * j));
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
